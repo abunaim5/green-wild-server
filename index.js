c089ae1efc2971f58dc3844cff3870d9ce49ceaa
Abu Naim <abunaim140@gmail.com>
2024-11-29 14:16:20 +0600

    added find all animals api with error handleing

--- index.js
+++ index.js
@@ -13,24 +13,37 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const animalCollection = client.db('greenWildDB').collection('animals');
+
+        // find all animals related api
+        app.get('/animals', async (req, res) => {
+            try {
+                const animals = await animalCollection.find().toArray();
+                res.status(200).json({ success: true, animals });
+            } catch (error) {
+                res.status(500).json({ success: false, message: 'failed to fetch animals' });
+            }
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
